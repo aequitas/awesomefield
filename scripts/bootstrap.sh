deb19371b97eab1c2dcb04521be300fa5406fb52
Johan <johan@Firefly.local>
2015-05-12 21:19:52 +0200

    Enable hiera-eyaml

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -8,4 +8,5 @@ wget http://apt.puppetlabs.com/puppetlabs-release-pc1-wheezy.deb
 sudo dpkg -i puppetlabs-release-pc1-wheezy.deb
 sudo apt-get update
 sudo apt-get -yqq install puppet
+sudo gem install hiera-eyaml
 touch ${skipfile}
---
+++ scripts/install_deps.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# install development dependencies
+
+sudo gem install hiera-eyaml librarian-puppet
