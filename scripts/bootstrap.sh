79512c8732c7da4e640f4c8c1a6579738b4dbd33
Johan <johan@Firefly.local>
2015-05-12 23:52:58 +0200

    Implement deep merge behaviour

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -8,5 +8,5 @@ wget http://apt.puppetlabs.com/puppetlabs-release-pc1-wheezy.deb
 sudo dpkg -i puppetlabs-release-pc1-wheezy.deb
 sudo apt-get update
 sudo apt-get -yqq install puppet
-sudo gem install hiera-eyaml
+sudo gem install hiera-eyaml deep_merge
 touch ${skipfile}
